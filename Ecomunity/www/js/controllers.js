8e98e67ad78c0208b9dbaa221d61f16c2d1c48fc
franheredia <franheredia.ov@gmail.com>
2016-11-16 13:38:28 -0300

    Renombrado 'anNimos' como 'anonimos'

--- Ecomunity/www/js/controllers.js
+++ Ecomunity/www/js/controllers.js
@@ -12,7 +12,7 @@ function ($scope, $stateParams) {
 
 }])
 
-.controller('anNimosCtrl', ['$scope', '$stateParams',
+.controller('anonimosCtrl', ['$scope', '$stateParams',
 function ($scope, $stateParams) {
 
 
